aef89ff29aa91fdc0dd00098203f0af7f0ee2559
ValenButtignol <valentinbuttignol@gmail.com>
2023-06-22 20:39:28 -0300

    continue minimization

--- src/algorithms/minimization.c
+++ src/algorithms/minimization.c
@@ -3,8 +3,15 @@
 #include "../../include/structures/dfa.h"
 #include "../../include/structures/node.h"
 #include "../../include/structures/set_of_markable_sets.h"
+#include "../../include/constants.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct MarkableSet {
+    int data;
+    int marked;
+    struct Node* next;
+} MarkableSet;
 
 DFA minimize(DFA automata) {
 
@@ -15,43 +22,140 @@ DFA minimize(DFA automata) {
         states = addNode(states, i);
         }
     }
-
     // create a list with the final states of automata: F
     Node* finalStates = automata.finalStates;
 
     // create a set of markable sets
-    SetOfMarkableSets* setOfMarkableSets;
-    // add the set Q\F to the set of markable sets
-    addSet(&setOfMarkableSets, states);
-    // add the set F to the set of markable sets
-    addSet(&setOfMarkableSets, finalStates);
-
+    SetOfMarkableSets* p;
+    SetOfMarkableSets* pDash;
     //create two markable sets
-    MarkableSet* X;
-    MarkableSet* Xp;
+    Node* x;
+    Node* xDash;
 
-    Node* e;
-    Node* ep;
+    // add the set Q\F to the set of markable sets
+    addSet(&p, states);
+    // add the set F to the set of markable sets
+    addSet(&p, finalStates);
 
-    // add to e and ep the states of the dfa
-    for (int i = 0; i < automata.numStates; i++) {
-        addData(&e, i);
-        addNode(&ep, i);
-    }
+    int changePartition = 0;
+    while (!changePartition) {
+        SetOfMarkableSets* currentSet = p;
 
+        while (currentSet != NULL) {
+            Node* x = currentSet->set;
 
-    int changed = 0;
-    while(!changed){
-        // iterate over P
-        for (SetOfMarkableSets* P = setOfMarkableSets; P != NULL; P = P->next) {
-            // iterate over e
-            for (Node* e = P->set; e != NULL; e = e->next) {
-                // check if e is marked and if X contains e
-                if (isMarked(X, e) && containsData(X, e->data)) {
-                    
+            while (x != NULL) {
+                int e = x->data;
+                if (isMarked(e, x)) {   // Este isMarked no está implementado.
+                    continue;
+                } else {
+                    markState(e, x);    // Este markState tampoco está implementado.
                 }
-            } 
+
+                addData(xDash, e, FALSE);   // Este addData tampoco está implementado.
+                
+                Node* xDashAux = xDash;
+                while (xDashAux != NULL) {
+                    int eDash = xDashAux->data;
+                    if (forAllWeirdFunction(automata, e, eDash)) {   // Este forAllWeirdFunction tampoco está implementado.
+                        markState(eDash, x);    // Este markState tampoco está implementado.
+                        addData(xDash, eDash, FALSE);   // Este addData tampoco está implementado.
+                    }
+                    xDashAux = xDashAux->next;
+                }
+                
+                addData(pDash, xDash, FALSE);
+                x = x->next;
+            }
+
+            currentSet = currentSet->next;
+            if (notEquals(p, pDash)) {   // Este notEquals
+                p = pDash;
+                pDash = NULL;   // delete all its elements
+            }
+            else {
+                changePartition = TRUE;
+            }
         }
     }
-
 }
+
+
+
+
+
+
+/***************************************/
+// create a list with the states of automata minus the final states: Q\F
+    Node* states;
+    for (int i = 0; i < automata.numStates; i++) {
+        if (!containsData(automata.finalStates, i)) {
+        states = addNode(states, i);
+        }
+    }
+    // create a list with the final states of automata: F
+    Node* finalStates = automata.finalStates;
+
+    // create a set of markable sets
+    SetOfMarkableSets* p;
+    SetOfMarkableSets* pDash;
+    //create two markable sets
+    Node* x;
+    Node* xDash;
+    // create states for the algorithm
+    int e;
+    int eDash;
+
+    // add the set Q\F to the set of markable sets
+    addSet(&p, states);
+    // add the set F to the set of markable sets
+    addSet(&p, finalStates);
+
+    int changePartition = 0;
+    while (!changePartition) {
+            SetOfMarkableSets* currentSet = p;
+
+            while (currentSet != NULL) {
+                // for each x in P
+                Node* x = currentSet->set;
+
+                // for each e in x
+                while (x != NULL) {
+                    int e = x->data;
+                    if (isMarked(e, x)) {   // Este isMarked no está implementado.
+                        continue;
+                    } else {
+                        markState(e, x);    // Este markState tampoco está implementado.
+                    }
+
+                    addData(xDash, e, false);   // Este addData tampoco está implementado.
+
+                    Node* xAux = x;
+                    while (xAux != NULL) {
+                        int eDash = x->data;
+
+                        if (forAllSymbols()) {
+                            markState(eDash, xAux);
+                            addData(xDash, eDash, false);
+                        }
+
+                        xAux = xAux->next;
+                    }
+
+                    addData(pDash, xDash, false);
+                    x = x->next;
+                }
+
+                if (p != pDash) {
+                    p = pDash;
+                    pDash = NULL;   // delete all its elements
+                }
+                else {
+                    changePartition = 1;
+                }
+
+                currentSet = currentSet->next;
+            }
+    }
+
+
