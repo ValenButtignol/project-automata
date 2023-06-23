72540aec7c7473a5ff8256734c53a32d6817b3f4
mateovieyra <mateovieyra8@gmail.com>
2023-06-22 23:02:50 -0300

    Add regex grammar

--- include/algorithms/parser.h
+++ include/algorithms/parser.h
@@ -1,24 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
-
 #include "../../include/structures/ndfa.h"
-
-/* 
-E->TE'
-E'->|TE'
-E'->-1
-T->FT'
-T'->.FT'
-T'->-1
-F->PF'
-F'->*
-F'->-1
-P->(E)
-P->L
-L->0
-L->1
-L->2
-*/
 
 int E(char** cursor, NDFA** ndfa);
 
